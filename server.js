0e8ea231d2b53e68176f02f9bb17ddd5afa6a768
ivancho72 <jorgeluna@inbox.com>
2020-05-02 21:51:40 -0400

    deployment adjustments

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 "use strict";
 const strapi = require("strapi/lib");
 const strapiDev = require("strapi/lib/commands/develop");
+const express = require("express");
 
-if (process.env.DEV_MODE === true) {
-  strapiDev();
+const app = express();
+
+if (process.env.DEMO_MODE === true) {
+  app.get("/", (req, res) => res.send("Hello World!"));
 } else {
-  strapi().start();
+  if (process.env.DEV_MODE === true) {
+    strapiDev();
+  } else {
+    strapi().start();
+  }
 }
